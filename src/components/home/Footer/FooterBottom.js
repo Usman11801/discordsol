5db6c551e9d241c0b0e40f7d1e9e544a364e5f3b
usman <muhammadusman@gsoftcosulting.com>
2024-09-09 00:57:24 +0500

    powerd

--- src/components/home/Footer/FooterBottom.js
+++ src/components/home/Footer/FooterBottom.js
@@ -12,7 +12,7 @@ const FooterBottom = () => {
           Copyright 2024 | DiscordSolutions shopping | All Rights Reserved |
           <a href="https://reactbd.com/" target="_blank" rel="noreferrer">
             <span className="ml-1 font-medium group-hover:text-primeColor">
-              Powered by Muhammad Usman
+              Powered by DiscordSolutions LLC
             </span>
           </a>
         </p>
